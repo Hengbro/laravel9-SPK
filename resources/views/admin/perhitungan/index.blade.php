680e29bc5cffe07af683d63688dcbd4e50a654a8
HengkiMac <hengkisaoremaha@gmail.com>
2024-10-15 23:59:26 +0700

    commit dua

--- resources/views/admin/perhitungan/index.blade.php
+++ resources/views/admin/perhitungan/index.blade.php
@@ -205,7 +205,7 @@
 </div>
 
 
-    <div class="card shadow mb-4">
+    <div class="card shadow mb-4 d-none">
         <!-- Card Header - Accordion -->
         <a href="#fuzzy" class="d-block card-header py-3" data-toggle="collapse"
                 role="button" aria-expanded="true" aria-controls="collapseCardExample">
